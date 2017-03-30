484320271b6b0cd01f5c558eca517e6e53125ce5
Kamil Gniazdowski <Kamil Gniazdowski>
2017-03-30 22:12:26 +0200

    Logowanie i dostęp do podglądu pożyczek tylko przez właściciela

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	before_filter :require_login
 
-  private
-  def current_user
-    User.where(id: session[:user_id]).first
-  end
-  helper_method :current_user
+	private
+
+	def require_login
+		unless current_user
+			redirect_to login_url
+		end
+	end
+	
+	def current_user
+		User.where(id: session[:user_id]).first
+	end
+	helper_method :current_user
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,18 @@
 class SessionsController < ApplicationController
+  skip_before_filter :require_login
   def new
   end
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'Logged in!'
+      redirect_to root_url, notice: 'Zalogowano!'
     else
       render :new
     end
   end
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: 'Logged out!'
+    redirect_to root_url, notice: 'Wylogowano!'
   end
 end
